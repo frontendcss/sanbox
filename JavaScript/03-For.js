38051d146806a7106cdb832e7126a59bdcf1910a
Limpus <fanklip.net@gmail.com>
2019-05-20 00:22:22 +0300

    JavaScript 2.1

--- JavaScript/03-For.js
+++ JavaScript/03-For.js
@@ -8,7 +8,7 @@ for(var anor = 1; anor <= olma; anor = anor + 1) {
 var totalPages = 5;
 
 for (var page = totalPages; page > 0; page = page - 1) {
-	console.log("Hello World!");
+	console.log("Печатаем страницы в обратном порядке");
 }
 
 /*
@@ -28,7 +28,7 @@ for (var page = totalPages; page > 0; page = page - 1) {
 var totalPages = 12;
 
 for (var page = 1; page <= totalPages; page += 2) {
-	console.log("Hello World!");
+	console.log("Печатаем только нечётные страницы");
 }
 
 var totalPages = 9;
