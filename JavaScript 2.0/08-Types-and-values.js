2aaa3ee365b755f09ddce22139f6b0d6acf9d956
Limpus <fanklip.net@gmail.com>
2020-06-03 17:28:29 +0300

    Числа

--- JavaScript 2.0/08-Types-and-values.js
+++ JavaScript 2.0/08-Types-and-values.js
@@ -1,4 +1,4 @@
-"use strict"
+// "use strict"
 
 // Типы
 // Встроенные типы
@@ -116,19 +116,19 @@
 // console.log(a.length);
 // console.log(b.length);
 
-// // simvolni pazitsyasi go'rsatadi
+// // simvolni pazitsyasini go'rsatadi
 // console.log(a.indexOf("f"));
 // console.log(b.indexOf("o"));
 
 // let c = a.concat("bar");
 // let d = b.concat(["b","a","r"]);
 
-// CтpoкиjavaScript неизменяемы, тогда как массивы вполне могут изменяться (мутировать)
+// // Cтpoки javaScript неизменяемы, тогда как массивы вполне могут изменяться (мутировать)
 // console.log(c === a); // false
 // console.log(d === b); // false
 
 // c = a.toUpperCase();
-// console.log(a === c);
+// console.log(a === c); // > false
 
 // b.push("!");
 // console.log(b);
@@ -138,9 +138,9 @@
 
 // // Перестановка строки в обратном поряд­ке
 // console.log(a.reverse); // > undefined
-// console.log(b.reverse());
+// console.log(b.reverse()); // !oof
 
-// let g = a.split(""); // разбиение ·а· на массив символов
+// let g = a.split(""); // разбиение а на массив символов
 // g.reverse(); // переставить массив символов в обратном порядке
 // g.join(""); // снова объединить массив символов в строку
 // console.log(g);
@@ -154,38 +154,136 @@
 // console.log(a.toExponential()); // > 5e+10
 
 // let a = 27.34;
-// console.log(a.toFixed(1)); // > 27.34 // type string!
+// console.log(a.toFixed(1)); // > 27.3 // type string!
 
-// console.log(27.toFixed(2)); // SyntaxError
+// // console.log(27.toFixed(2)); // SyntaxError
 // console.log((27).toFixed(1)); // > 27.0
 // console.log(27..toFixed(2)); // > 27.00
 // console.log(27.00.toFixed(1)); // > 27.0
 
-// Малые дробные значения
+// // Малые дробные значения
 // let a = 0.1 + 0.2;
 // console.log(a.toFixed(17)); // 0.1 + 0.2 === 0.3 > false
 
-// Проверка целых чисел
+// // Проверка целых чисел
 
-// является ли значение целым числом
-console.log(Number.isInteger(42)); // true
-console.log(Number.isInteger(42.000)); // true
-console.log(Number.isInteger(27.7)); // false
+// // является ли значение целым числом
+// console.log(Number.isInteger(42)); // true
+// console.log(Number.isInteger(42.000)); // true
+// console.log(Number.isInteger(27.7)); // false
 
-// является ли значение безопасным целым чис­лом
+// // является ли значение безопасным целым чис­лом
+// console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)); // true > 9007199254740991
+// console.log(Number.isSafeInteger(Math.pow(2,53))); // false > 9007199254740992
 
-console.log(Number.isSafeInteger(Number.MAX_SAFE_INTEGER)); // true
-console.log(Number.isSafeInteger(Math.pow(2,53))); // false
+// Оператор void
+// Выражение void «стирает» любое значение,так что результат выражения всегда является неопределенным
+// let a = 27;
+// console.log(void a, a); // undefined 27
 
+// Тип number включает несколько специальных значений
+// NaN
 
+// let a = "Limpus" / 27;
+// let b = "foo";
 
+// console.log(a); // > NaN (Not А Number)
+// console.log(typeof a === "number"); // > true
 
+// console.log(a === NaN); // false
+// console.log(a == NaN); // false
 
+// // Проверку на NaN
+// console.log(isNaN(a)); // > true
+// console.log(isNaN(b)); // > true Ups!
 
+// console.log(Number.NaN);
 
+// Бесконечности
+// let a = 1 / 0;
+// let b = -1 / 0;
 
+// console.log(a); // > Infinity
+// console.log(b); // > -Infinity
 
+// console.log(Number.POSITIVE_INFINITY); // > Infinity
+// console.log(Number.NEGATIVE_INFINITY); // > -Infinity
 
+// let a = Number.MAX_VALUE;
+// console.log(a + a); // > Infinity
+
+// let b = (Infinity / Infinity);
+// console.log(b); // > NaN
+
+// let s = 27 / Infinity;
+// console.log(s); // > 0
+
+// let t = -27 / Infinity;
+// console.log(t); // > -0
+
+// Нули
+
+// let a = 0 / -3;
+// let b = 0 * -3;
+// console.log(a); // > -0
+// console.log(b); // > -0
+
+// console.log(a.toString()); // > 0
+
+// let a = 0 / -3;
+// let b = 0;
+
+// console.log(a == b); // true
+// console.log(a === b); // true
+
+// console.log(0 == -0); // true
+// console.log(0 === -0); // true
+
+// console.log(0 > -0); // false
+
+// Специальное равенство
+
+// let a = 2 / "foo";
+// let b = -3 * 0;
+
+// console.log(a); // > NaN
+// console.log(b); // > -0
+
+// console.log(Object.is(a, NaN)); // true
+// console.log(Object.is(b, 0)); // false
+
+// Значения и ссылки
+
+// let a = 27;
+// let b = a; // 'Ь' всегда содержит копию значения из 'а'
+// b++;
+
+// console.log(a); // > 27
+// console.log(b); // > 28
+
+// let c = [1, 2, 3];
+// let d = c; // 'd' ссылка на общее значение [1,2,3]
+// d.push(4,5);
+
+// console.log(c); // [1,2,3,4,5]
+// console.log(d); // [1,2,3,4,5]
+
+// d = [6,7,8,9,10]
+// console.log(d); // [6,7,8,9,10]
+
+function foo(mass){
+	mass.push(4);
+	mass.length = 0;
+
+	mass.push(5,6,7,8);
+	console.log(mass);
+}
+
+let a = [1,2,3];
+
+foo(a.slice());
+
+console.log(a);
 
 
 
