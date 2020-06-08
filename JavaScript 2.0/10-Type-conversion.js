e1f956972b1e58db79c3e454c790893a1fd46567
Limpus <fanklip.net@gmail.com>
2020-06-08 19:46:39 +0300

    Явные преобразования

--- JavaScript 2.0/10-Type-conversion.js
+++ JavaScript 2.0/10-Type-conversion.js
@@ -18,8 +18,8 @@
 // console.log(d); // > string
 
 // let a = JSON.stringify(undefined);
-// let b = JSON.stringify([1, function(){}, 2]);
-// let d = JSON.stringify({a:1, b:function(){}, d:2});
+// let b = JSON.stringify([1, function(){}, 2]); // massivni ichinda chiqsa o'rnina null chiqadi
+// let d = JSON.stringify({a:1, b:function(){}, d:2}); // objectda chiqsa o'tgazib yuboriladi
 // console.log(a);
 // console.log(b);
 // console.log(d);
@@ -32,10 +32,10 @@
 // 	c: function(){}
 // };
 
-// // создать циклическую ссылку в > а
-// o.e = obj;
+// // // создать циклическую ссылку в > а
+// // o.e = obj;
 
-// //выдаст ошибку из-за циклической ссылки
+// // // выдаст ошибку из-за циклической ссылки
 // // JSON.stringify(obj);
 
 // // определить нестандартную сериализацию значения JSON
@@ -48,7 +48,6 @@
 
 // let result = JSON.stringify(obj);
 // console.log(result);
-
 
 // let obj = {
 // 	b: 27,
@@ -63,32 +62,67 @@
 // 	if(k !== "c"){
 // 		return v;
 // 	}
-// });s
+// });
 // console.log(b);
 
 // let d = JSON.stringify(obj, null, 3); // "----"
 // console.log(d);
 
+// ToNumber
 
+// let a = true == 1;
+// console.log(a); // > true
 
+// ToBoolean
+// console.log(Boolean(undefined)); // false
+// console.log(Boolean(null)); // false
+// console.log(Boolean(false)); // false
+// console.log(Boolean("")); // false
+// console.log(Boolean(+0)); // false
+// console.log(Boolean(-0)); // false
+// console.log(Boolean(NaN)); // false
 
+// console.log(Boolean("''")); // > true
+// console.log(Boolean(obj = {})); // > true
 
+// Ложные объекты
 
+// let a = new Boolean(false);
+// let b = new Boolean(0);
+// let c = new Boolean("");
 
+// let d = Boolean(a && b && c);
+// console.log(d); // > true
 
+// Истинные значения
 
+// let a = {};
+// let b = [];
+// let d = function(){};
 
+// let result = Boolean(a && b && d);
+// console.log(result); // true!
 
+// Явные преобразования
+ // Number <--> String
+// let a = 27;
+// // let b = String(a);
+// let b = a.toString();
+// console.log(typeof b); // type string!
 
+// String <--> Number
+// let a = "27";
+// // let b = Number(a);
+// let b = +a;
+// console.log(typeof b); // > type number!
 
+let a = "1.5";
+let b = 1+ +a;
+console.log(b); // > 2.5
 
-
-
-
-
-
-
-
+// let a = "1.5";
+// let b = 1+ -a;
+// console.log(b); // > -0.5
 
 
 
